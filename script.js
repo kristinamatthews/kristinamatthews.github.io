aacbfc8480ddd93ce0c73d7d1785c970025dd034
kristinamatthews <54610118+kristinamatthews@users.noreply.github.com>
2019-11-20 13:33:58 -0500

    Assignment 16

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-async function showAthletes(){
+/*async function showAthletes(){
     console.log("hello world");
-    let response = await fetch("/api/athletes");
+    let response = await fetch('/api/athletes');
     let athletesJson = await response.json();
     console.log(athletesJson);
 
@@ -17,33 +17,159 @@ async function showAthletes(){
         console.log(athlete);
         contentDiv.append(makeAthleteElem(athlete));
     }
+}*/
+
+async function showAthletes(){
+    let athletesJson = await fetch('api/athletes');
+    let athletes = await athletesJson.json();
+    let athletesDiv = document.getElementById("content");
+    athletesDiv.innerHTML = "";
+
+    for(i in athletes){
+        athletesDiv.append(makeAthleteElem(athletes[i]));
+    }
 }
 
+async function showAthlete(){
+    let id = document.getElementById("input-athlete-id").value;
+    let response = await fetch(`api/athletes/${id}`);
+    let athlete = await response.json();
 
+    let aElem = document.getElementById("athlete");
+    aElem.append(makeAthleteElem(athlete));
+}
 
 function makeAthleteElem(athlete){
     let aElem = document.createElement("div");
     aElem.classList.add("athlete");
     aH2 = document.createElement("h2");
-    aH2.innerHTML = athlete.firstname + athlete.lastname;
+    aH2.innerHTML = athlete.firstname +" " + athlete.lastname;
     aElem.append(aH2);
     aP = document.createElement("p");
-    aP.innerHTML = `Athlete ${athlete.firstname} ${athlete.lastname} is a ${athlete.league} ${athlete.sport} player for the ${athlete.team} playing the position of ${athlete.position}. His/Her birthday is ${athlete.birthday}.`;
+    aP.innerHTML = `Athlete ${athlete.firstname} ${athlete.lastname} is a ${athlete.league} ${athlete.sport} player for the ${athlete.team}.`;
     aElem.append(aP);
-    aImage = document.createElement("img");
-    console.log(athlete.picture);
-    console.log("http://localhost:4000/" + athlete.picture);
-    aImage.src = "http://localhost:4000/" + athlete.picture;
-    aElem.append(aImage);
+
+    
+    //create edit and delete links
+    let editLink = document.createElement("a");
+    editLink.href = "#edit-athlete-form";
+    editLink.innerHTML = "Edit";
+    editLink.setAttribute("data-id", athlete.id);
+    editLink.onclick =showEditAthlete; //pay attention
+    let deleteLink = document.createElement("a");
+    deleteLink.href = "#";
+    deleteLink.innerHTML = "Delete";
+    deleteLink.setAttribute("data-id", athlete.id); //create athlete id 
+    deleteLink.onclick = deleteAthlete; //pay attention
+    aP.append(editLink);
+    aP.append(deleteLink);
+
+    aElem.append(aH2);
+    aElem.append(aP);
 
 
     return aElem;
+}
+
+async function showEditAthlete(){
+    const id = this.getAttribute("data-id");
+    document.getElementById("edit-athlete-id").innerHTML = id;
+
+    let response = await fetch(`api/athletes/${id}`);
+    let athlete = await response.json();
+    document.getElementById("edit-athlete-first-name").value = athlete.firstname;
+    document.getElementById("edit-athlete-last-name").value = athlete.lastname;
+    document.getElementById("edit-athlete-sport-name").value = athlete.sport;
+    document.getElementById("edit-athlete-league-name").value = athlete.league;
+    document.getElementById("edit-athlete-team-name").value = athlete.team;
+
+    return false;
+}
+
+async function deleteAthlete(){
+    const id = this.getAttribute("data-id");
+    
+    let response = await fetch(`/api/athletes/${id}`, {
+        method: 'DELETE',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        }
+    });
+
+    if(response.status != 200){
+        console.log("Error deleting athlete");
+        return;
+    }
+
+    showAthletes();
+    return false;
+}
+
+async function addAthlete(){
+    //get the athlete inforamtion
+    const FirstName = document.getElementById("athlete-first-name").value;
+    const LastName = document.getElementById("athlete-last-name").value;
+    const SportName = document.getElementById("athlete-sport-name").value;
+    const LeagueName = document.getElementById("athlete-league-name").value;
+    const TeamName = document.getElementById("athlete-team-name").value;
+
+    console.log(`you are adding ${FirstName}, ${LastName}, ${SportName}, ${LeagueName}, ${TeamName}`);
+
+    let newAthlete = {"FirstName":FirstName, "LastName":LastName, "SportName":SportName, "LeagueName":LeagueName, "TeamName":TeamName};
+
+    let response = await fetch('/api/athletes/', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        },
+        body:JSON.stringify(newAthlete),
+    });
+
+    if(response.status != 200){
+        console.log("Error adding athlete");
+        return;
+    }
+
+    let result = await response.json();
+    showAthletes();
+}
+
+async function editAthlete(){
+    let id = document.getElementById("edit-athlete-id").textContent;
+    let FirstName = document.getElementById("edit-athlete-first-name").value;
+    let LastName = document.getElementById("edit-athlete-last-name").value;
+    let SportName = document.getElementById("edit-athlete-sport-name").value;
+    let LeagueName = document.getElementById("edit-athlete-league-name").value;
+    let TeamName = document.getElementById("edit-athlete-team-name").value;
+    let athlete = {"First Name":FirstName, "Last Name":LastName, "Sport Name":SportName, "League Name":LeagueName, "Team Name":TeamName};
+
+    let response = await fetch(`/api/athletes/${id}`, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(athlete),
+    });
+
+    if(response.status != 200){
+        console.log("Error edditing athlete");
+    }
+
+    //update the list of athletes
+    showAthletes();
 }
 
 
 
 
 window.onload = function() {
-    console.log("Hello World");
     this.showAthletes();
+    let addAthleteButton = document.getElementById("btn-add-athlete");
+    addAthleteButton.onclick = addAthlete;
+
+    let showAthleteButton = document.getElementById("btn-show-athlete");
+    showAthleteButton.onclick = showAthlete;
+
+    let editAthleteButton = document.getElementById("btn-edit-athlete");
+    editAthleteButton.onclick = editAthlete;
 }
--- server.js
+++ server.js
@@ -5,61 +5,114 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+const athletes = [
+    {id:1,firstname:"Russell",lastname:"Westbrook",sport:"Basketball",league:"NBA",team:"Houston Rockets"},
+    {id:2,firstname:"Kevin",lastname:"Durant",sport:"Basketball",league:"NBA",team:"Brooklyn Nets"},
+    {id:3,firstname:"Skylar",lastname:"Diggins-Smith",sport:"Basketball",league:"WNBA",team:"Dallas Wings"},
+    {id:4,firstname:"Tom",lastname:"Brady",sport:"Football",league:"NFL",team:"New England Patriots"},
+    {id:5,firstname:"A'ja",lastname:"Wilson",sport:"Basketball",league:"WNBA",team:"Las Vegas Aces"},
+    {id:6,firstname:"Kobe",lastname:"Bryant",sport:"Basketball",leauge:"NBA",team:"LA Lakers"},
+]
+
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/api/athletes',(req,res)=>{
-    res.send(allAthletes);
+    res.send(athletes);
 });
 
-app.post('/api/athletes', (req,res)=>{
-    const schema = {
-        FirstName:Joi.string().min(3).required(),
-        LastName:Joi.string().min(4).required(),
-        SportName:Joi.string().required(),
-        LeagueName:Joi.string().required(),
-        TeamName:Joi.string().required(),
+app.get('/api/athletes/:id', (req,res)=>{
+    const requestedId = parseInt(req.params.id);
+    const athlete = athletes.find(s =>s.id === requestedId);
+
+    if(!athlete) {
+        res.status(404).send(`The athlete with id ${requestedId} can not be found`);
+        return;
     }
 
-    const result = Joi.validate(req.body, schema);
+    res.send(athlete);
+});
+
+function validateAthlete(athlete){
+    const schema = {
+        firstname:Joi.string().min(3).required(),
+        lastname:Joi.string().min(4).required(),
+        sport:Joi.string().required(),
+        league:Joi.string().required(),
+        team:Joi.string().required(),
+    }
+
+    return Joi.validate(athlete, schema);
+}
+
+app.post('/api/athletes', (req,res)=>{
+
+    const result = validateAthlete(req.body);
 
     if(result.error){
         res.status(400).send(result.error.details[0].message);
     }
 
     const athlete = {
-        id:allAthletes.length + 1,
-        FirstName : req.body.FirstName,
-        LastName : req.body.LastName,
-        SportName : req.body.SportName,
-        LeagueName : req.body.LeagueName,
-        TeamName : req.body.TeamName,
+        id:athletes.length + 1,
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        sport:req.body.sport,
+        league:req.body.league,
+        team:req.body.team
 
     }
-    console.log("name is: " + req.body.name);
     athletes.push(athlete);
     res.send(athlete);
 });
 
+//update an athlete
+app.put('/api/athletes/:id', (req,res)=>{
+    const requestedId = parseInt(req.params.id);
+    const athlete = athletes.find(s =>s.id === requestedId);
+
+    if(!athlete) {
+        res.status(404).send(`The athlete with id ${requestedId} can not be found`);
+        return;
+    }
+
+    const result = validateAthlete(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    //update
+    athlete.firstname = req.body.firstname;
+    athlete.lastname = req.body.lastname;
+    athlete.sport = req.body.sport;
+    athlete.league = req.body.league;
+    athlete.team = req.body.team;
+    res.send(athlete);
+
+});
+
+app.delete('/api/athletes/:id',(req,res)=>{
+    const requestedId = parseInt(req.params.id);
+    const athlete = athletes.find(s =>s.id === requestedId);
+
+    if(!athlete) {
+        res.status(404).send(`The athlete with id ${requestedId} can not be found`);
+        return;
+    }
+
+    //if it does exist, go and delete it
+    let index = athletes.indexOf(athlete);
+    athletes.splice(index,1);
+    res.send(athlete);
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//listening 
 const port = process.env.PORT || 4000;
-
 app.listen(port,()=>{
     console.log(`listening on port ${port}...`);
 });
